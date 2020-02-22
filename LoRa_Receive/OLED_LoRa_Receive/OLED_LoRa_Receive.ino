586c15050d1eace61169a9042c34f6f8b045b654
Ronnie R. Winter <ro2nie@gmail.com>
2020-02-22 21:32:33 +0000

    Connects to wifi and mqtt server. Sends water level to mqtt srvr

--- LoRa_Receive/OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ LoRa_Receive/OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <LoRa.h>
-#include <Wire.h>  
+#include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISO
@@ -10,22 +12,74 @@
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND    868E6
 
+//LoRa
 String rssi = "RSSI --";
 String packSize = "--";
-String packet ;
+String packet;
+//WiFi
+const char* ssid     = "<replace with SSID>";
+const char* password = "<replace with passphrase>";
+const char* mqttServer = "<replace with IP or domain>";
+const char* waterTankLevelTopic = "water-tank/level";
 
-void loraData(){
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void initWiFi() {
+  // Start by connecting to a WiFi network
+  Serial.println("Connecting to: " + String(ssid));
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void initMqtt() {
+  client.setServer(mqttServer, 1883);
+  //  client.setCallback(callback);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ES32-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loraData() {
   Serial.print(packSize + " bytes");
   Serial.print(" - ");
   Serial.print(packet);
   Serial.print(" -- ");
   Serial.println(rssi);
+  client.publish(waterTankLevelTopic, packet.c_str());
+  //  return packet
 }
 
 void cbk(int packetSize) {
-  packet ="";
-  packSize = String(packetSize,DEC);
-  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  packet = "";
+  packSize = String(packetSize, DEC);
+  for (int i = 0; i < packetSize; i++) {
+    packet += (char) LoRa.read();
+  }
   rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
   loraData();
 }
@@ -35,8 +89,8 @@ void setup() {
   while (!Serial);
   Serial.println();
   Serial.println("LoRa Receiver Callback");
-  SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);  
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DI0);
   if (!LoRa.begin(868E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
@@ -44,11 +98,22 @@ void setup() {
   //LoRa.onReceive(cbk);
   LoRa.receive();
   Serial.println("init ok");
+
+  //WiFi
+  initWiFi();
+  initMqtt();
   delay(1500);
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
   int packetSize = LoRa.parsePacket();
-  if (packetSize) { cbk(packetSize);  }
+  if (packetSize) {
+    cbk(packetSize);
+  }
   delay(10);
 }
